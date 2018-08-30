a791c089b8e63f28de7e9416517154582b34610a
Rubén Ruiz Marrón <rubenruizmarron@gmail.com>
2018-08-30 00:10:41 -0500

    Jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,8 +5,14 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
-    public float speed = 200f;
+    //Config
+    [SerializeField] float speed = 2f;
+    [SerializeField] float jump = 2f;
 
+    // State
+    public bool isAlive = true;
+
+    // Component references
     private Rigidbody2D myRigidbody;
     private Animator myAnimator;
 
@@ -21,27 +27,34 @@ public class Player : MonoBehaviour {
 	void Update ()
     {
         Run();
+        Jump();
     }
 
     private void Run()
     {
-        if (CrossPlatformInputManager.GetAxis("Horizontal") > 0.1f || CrossPlatformInputManager.GetAxis("Horizontal") < -0.1f)
+        float horizontalInput = CrossPlatformInputManager.GetAxis("Horizontal");
+        bool isMovingHorizontaly = horizontalInput < -0.1f || horizontalInput > 0.1f;
+
+        // Moves player character left or right
+        if (isMovingHorizontaly)
         {
-            myRigidbody.velocity = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal") * speed * Time.deltaTime, myRigidbody.velocity.y);
+            myRigidbody.velocity = new Vector2(horizontalInput * speed, myRigidbody.velocity.y);
             myAnimator.SetBool("Running", true);
+
+            // Flips sprite on the direction of movement
+            transform.localScale = new Vector3(Mathf.Sign(myRigidbody.velocity.x), 1f);
         }
         else
         {
             myAnimator.SetBool("Running", false);
         }
+    }
 
-        if (CrossPlatformInputManager.GetAxis("Horizontal") < -0.1f)
+    private void Jump()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
         {
-            transform.localScale = new Vector3(-1, 1);
-        }
-        else if (CrossPlatformInputManager.GetAxis("Horizontal") > 0.1f)
-        {
-            transform.localScale = new Vector3(1, 1);
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, +jump);
         }
     }
 }
