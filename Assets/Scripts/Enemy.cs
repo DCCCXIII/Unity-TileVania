8b34d4fadb4778c6bc6061a9fca048bd9f80ff69
Rubén Ruiz Marrón <rubenruizmarron@gmail.com>
2018-09-05 10:22:11 -0500

    Added enemy, moves

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] float speed = 1.0f;
+
+    public bool isMovingLeft = false;
+
+    private Rigidbody2D myRigidbody;
+    private Collider2D myCollider;
+    private Collider2D sidesCollider;
+
+	// Use this for initialization
+	void Start () {
+        myCollider = GetComponent<CircleCollider2D>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        sidesCollider = GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Move();
+        ToggleIsMovingLeft();
+    }
+
+    private void Move()
+    {
+        if (!isMovingLeft)
+        {
+            myRigidbody.velocity = new Vector2(speed * Time.deltaTime, myRigidbody.velocity.y);
+        } else if (isMovingLeft)
+        {
+            myRigidbody.velocity = new Vector2(speed * Time.deltaTime * -1, myRigidbody.velocity.y);
+        }
+
+    }
+
+    private void ToggleIsMovingLeft()
+    {
+        if (sidesCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            if (isMovingLeft)
+            {
+                isMovingLeft = false;
+            } else if (!isMovingLeft)
+            {
+                isMovingLeft = true;
+            }
+        }
+    }
+}
