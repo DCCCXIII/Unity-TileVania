a53f61ea94583f1b93bd0355e6a604167ea7080a
Rubén Ruiz Marrón <rubenruizmarron@gmail.com>
2018-12-03 13:12:34 -0600

    Added exit level object

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour {
+
+    [SerializeField] float levelLoadWait = 1f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            StartCoroutine(LoadNextLevel());
+        }   
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSeconds(levelLoadWait);
+
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
