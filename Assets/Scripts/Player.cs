c0da4c80e8eb9232cf5123d12748cfc493c46e5d
Rubén Ruiz Marrón <rubenruizmarron@gmail.com>
2018-12-02 22:16:06 -0600

    Added Hazards

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -93,7 +93,7 @@ public class Player : MonoBehaviour {
 
     private void Die()
     {
-        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemy")))
+        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemy", "Hazards")))
         {
             myAnimator.SetTrigger("Dying");
             GetComponent<Rigidbody2D>().velocity = deathKick;
